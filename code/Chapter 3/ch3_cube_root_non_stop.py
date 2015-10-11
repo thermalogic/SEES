ebbc21a2303f807819d9a1283f037a9338581b34
hydro <hydro@Hydro>
2015-10-11 13:46:27 +0800

    10.11

--- code/Chapter 3/ch3_cube_root_non_stop.py
+++ code/Chapter 3/ch3_cube_root_non_stop.py
@@ -1,12 +1,12 @@
 x=8
 ans = 0  
 while ans**3 < abs(x):
-    print('Value of the decrementing function abs(x) - ans**3 is',\
+    print('Value of the decrementing function abs(x) - ans**3 is',
           abs(x) - ans**3)  # add the statement at the start of the loop
                             # test whether the decrementing function is indeed being decremented
     ans = ans  # replace the statement ans = ans + 1 by ans = ans 
-               # 4.Its value is decreased every time through the loop. 
-               # 3.When its value is <=0, the loop terminates. 
+               # Its value is decreased every time through the loop. 
+               # When its value is <=0, the loop terminates. 
 
 if ans**3 != abs(x):
     print(x, 'is not a perfect cube')
