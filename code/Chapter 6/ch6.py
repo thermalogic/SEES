18a920cda914d14f368d5ff6ae42de99a56100bd
hydro <hydro@Hydro>
2015-10-19 12:08:44 +0800

    update chapter 6

---
+++ code/Chapter 6/ch6.py
@@ -0,0 +1,80 @@
+﻿# Page 73
+def copy(L1, L2):
+    """Assumes L1, L2 are lists
+       Mutates L2 to be a copy of L1"""
+    while len(L2) > 0:  # remove all elements from L2
+        L2.pop()  # remove last element of L2
+    for e in L1:  # append L1's elements to initially empty L2
+        L2.append(e)
+
+def isPrime(x):
+    """Assumes x is a nonnegative int
+       Returns True if x is prime; False otherwise"""
+    if x <= 2:
+        return False
+    for i in range(2, x):
+        if x % i == 0:
+            return False
+    return True
+
+# Page 74
+def abs(x):
+    """Assumes x is an int
+       Returns x if x>=0 and –x otherwise"""
+    if x < -1:
+        return -x
+    else:
+        return x
+
+# Page 79, Figure 6.1
+def isPal(x):
+    """Assumes x is a list
+       Returns True if the list is a palindrome; False otherwise"""
+    temp = x
+    temp.reverse
+    if temp == x:
+        return True
+    else:
+        return False
+
+def silly(n):
+    """Assumes n is an int > 0
+       Gets n inputs from user
+       Prints 'Yes' if the sequence of inputs forms a palindrome;
+       'No' otherwise"""
+    for i in range(n):
+        result = []
+        elem = input('Enter element: ')
+        result.append(elem)
+    if isPal(result):
+        print('Yes')
+    else:
+        print('No')
+
+# Page 80
+def silly(n):
+    """Assumes n is an int > 0
+       Gets n inputs from user
+       Prints 'Yes' if the sequence of inputs forms a palindrome;
+           'No' otherwise"""
+    result = []
+    for i in range(n):
+        elem = eval(input('Enter element: '))
+        result.append(elem)
+    
+    print(result)
+    if  isPal(result):
+        print('Yes')
+    else:
+        print('No')
+
+# Page 81
+def isPal(x):
+    """Assumes x is a list
+       Returns True if the list is a palindrome; False otherwise"""
+    temp = x[:]
+    temp.reverse()
+    if temp == x:
+        return True
+    else:
+        return False
