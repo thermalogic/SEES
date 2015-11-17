afd0e70dcbff8b084f02f811c7cbf2a315ff8cbf
hydro <hydro@Hydro>
2015-11-17 16:56:42 +0800

    update pythontools

--- code/Chapter26.3/doctest_ellipsis.py
+++ code/Chapter26.3/doctest_ellipsis.py
@@ -4,7 +4,7 @@ class MyClass(object):
 def unpredictable(obj):
     """Returns a new list containing obj.
 
-    >>> unpredictable(MyClass())
-    [<doctest_unpredictable.MyClass object at 0x10055a2d0>]
+    >>> unpredictable(MyClass()) #doctest: +ELLIPSIS
+    [<doctest_ellipsis.MyClass object at 0x...>]
     """
     return [obj]
