6bfc764c579f85f1c2ee370d38a0a5e3fe7c8336
hydro <hydro@Hydro>
2016-02-18 09:35:59 +0800

    update r1

--- code/Chapter4/ch4_code.py
+++ code/Chapter4/ch4_code.py
@@ -1,46 +1,51 @@
-#Page 34, figure 4.1
+# Page 34, figure 4.1
 x = 25
 epsilon = 0.01
 numGuesses = 0
 low = 0.0
 high = max(1.0, x)
-ans = (high + low)/2.0
+ans = (high + low) / 2.0
 while abs(ans**2 - x) >= epsilon:
     numGuesses += 1
     if ans**2 < x:
         low = ans
     else:
         high = ans
-    ans = (high + low)/2.0
+    ans = (high + low) / 2.0
 print('numGuesses =', numGuesses)
 print(ans, 'is close to square root of', x)
 
-#Page 35
+# Page 35
+
+
 def max(x, y):
     if x > y:
         return x
     else:
         return y
 
-#Page 36, Figure 4.2
-def printName(firstName, lastName, reverse): 
+# Page 36, Figure 4.2
+
+
+def printName(firstName, lastName, reverse):
    if reverse:
       print(lastName + ', ' + firstName)
    else:
       print(firstName, lastName)
 
-#Page 36
+# Page 36
 printName('Olga', 'Puchmajerova', False)
 printName('Olga', 'Puchmajerova', False)
-printName('Olga', 'Puchmajerova', reverse = False)
-printName('Olga', lastName = 'Puchmajerova', reverse = False)
+printName('Olga', 'Puchmajerova', reverse=False)
+printName('Olga', lastName='Puchmajerova', reverse=False)
 printName(lastName='Puchmajerova', firstName='Olga', reverse=False)
 
-#Page 37
+# Page 37
 
-printName('Olga', lastName = 'Puchmajerova', False)
+printName('Olga', lastName='Puchmajerova', False)
 
-def printName(firstName, lastName, reverse = False):
+
+def printName(firstName, lastName, reverse=False):
    if reverse:
       print(lastName + ', ' + firstName)
    else:
@@ -48,10 +53,12 @@ def printName(firstName, lastName, reverse = False):
 
 printName('Olga', 'Puchmajerova')
 printName('Olga', 'Puchmajerova', True)
-printName('Olga', 'Puchmajerova', reverse = True)
+printName('Olga', 'Puchmajerova', reverse=True)
 
 # 4.1.3 Scoping
-def f(x): #name x used as formal parameter
+
+
+def f(x):  # name x used as formal parameter
     y = 1
     x = x + y
     print('x =', x)
@@ -59,24 +66,27 @@ def f(x): #name x used as formal parameter
 
 x = 3
 y = 2
-z = f(x) #value of x used as actual parameter
-print( 'z =', z)
-print( 'x =', x)
-print( 'y =', y)
+z = f(x)  # value of x used as actual parameter
+print('z =', z)
+print('x =', x)
+print('y =', y)
 
-#Page 38, Figure 4.3
+# Page 38, Figure 4.3
+
+
 def f(x):
    def g():
       x = 'abc'
-      print( 'x =', x)
+      print('x =', x)
+
    def h():
       z = x
-      print( 'z =', z)
+      print('z =', z)
    x = x + 1
-   print( 'x =', x)
+   print('x =', x)
    h()
    g()
-   print( 'x =', x)
+   print('x =', x)
    return g
 
 x = 3
@@ -85,9 +95,12 @@ print('x =', x)
 print('z =', z)
 z()
 
-#Page 40
+# Page 40
+
+
 def f():
    print(x)
+
 
 def g():
    print(x)
@@ -98,24 +111,27 @@ f()
 x = 3
 g()
 
-#Page 42, Figure 4.5
+# Page 42, Figure 4.5
+
+
 def findRoot(x, power, epsilon):
     """Assumes x and epsilon int or float, power an int,
            epsilon > 0 & power >= 1
        Returns float y such that y**power is within epsilon of x.
            If such a float does not exist, it returns None"""
-    if x < 0 and power%2 == 0:
+    if x < 0 and power % 2 == 0:
         return None
     low = min(-1.0, x)
     high = max(1.0, x)
-    ans = (high + low)/2.0
+    ans = (high + low) / 2.0
     while abs(ans**power - x) >= epsilon:
         if ans**power < x:
             low = ans
         else:
             high = ans
-        ans = (high + low)/2.0
+        ans = (high + low) / 2.0
     return ans
+
 
 def testFindRoot():
     epsilon = 0.0001
@@ -125,11 +141,13 @@ def testFindRoot():
                   ' and power = ' + str(power))
             result = findRoot(x, power, epsilon)
             if result == None:
-                print( '   No root')
+                print('   No root')
             else:
                 print('   ', result**power, '~=', x)
 
-#Page 45, Figure 4.6
+# Page 45, Figure 4.6
+
+
 def factI(n):
    """Assumes that n is an int > 0
       Returns n!"""
@@ -138,35 +156,41 @@ def factI(n):
       result = result * n
       n -= 1
    return result
-   
+
+
 def factR(n):
    """Assumes that n is an int > 0
       Returns n!"""
    if n == 1:
       return n
    else:
-       return n*factR(n - 1)
+       return n * factR(n - 1)
 
-#Page 47, Figure 4.7
+# Page 47, Figure 4.7
+
+
 def fib(n):
     """Assumes n an int >= 0
        Returns Fibonacci of n"""
     if n == 0 or n == 1:
         return 1
     else:
-        return fib(n-1) + fib(n-2)
+        return fib(n - 1) + fib(n - 2)
+
 
 def testFib(n):
-    for i in range(n+1):
-        print( 'fib of', i, '=', fib(i))
+    for i in range(n + 1):
+        print('fib of', i, '=', fib(i))
 
-#Page 48, Figure 4.8
+# Page 48, Figure 4.8
+
+
 def isPalindrome(s):
     """Assumes s is a str
       Returns True if s is a palindrome; False otherwise.
         Punctuation marks, blanks, and capitalization are
         ignored."""
-   
+
     def toChars(s):
       s = s.lower()
       letters = ''
@@ -184,10 +208,10 @@ def isPalindrome(s):
          answer = s[0] == s[-1] and isPal(s[1:-1])
          print('  About to return', answer, 'for', s)
          return answer
-         
+
    return isPal(toChars(s))
 
-#page 49, Figure 4.9
+# page 49, Figure 4.9
 def isPalindrome(s):
    """Assumes s is a str
       Returns True if s is a palindrome; False otherwise.
@@ -220,7 +244,7 @@ def testIsPalindrome():
    print('Try doGood')
    print (isPalindrome('doGood'))
 
-#Page 51, Figure 4.10
+# Page 51, Figure 4.10
 def fib(x):
     """Assumes x an int >= 0
        Returns Fibonacci of x"""
@@ -238,9 +262,9 @@ def testFib(n):
         print('fib of', i, '=', fib(i))
         print('fib called', numFibCalls, 'times.')
 
-#Page 52, See circle.py
+# Page 52, See circle.py
 
-#Page 52
+# Page 52
 import circle
 print( circle.pi)
 print( circle.area(3))
@@ -251,14 +275,14 @@ from circle import *
 print(pi)
 print(circle.pi)
 
-#Page 53
+# Page 53
 nameHandle = open('kids', 'w')
 for i in range(2):
     name = input('Enter name: ')
     nameHandle.write(name + '\n')
 nameHandle.close()
 
-#Page 54
+# Page 54
 nameHandle = open('kids', 'r')
 for line in nameHandle:
     print(line[:-1])
--- code/PyTurbine/PyCylinderEff.py
+++ code/PyTurbine/PyCylinderEff.py
@@ -1,6 +1,5 @@
 #coding: utf-8 -*- 
 
-#coding: utf-8 -*- 
 # HP of 300MW Unit
 #    LOAD 252.44 MW
 #    QMS 750.19 t/h
@@ -26,14 +25,14 @@ h2=pt2h(p2,t2)
 s2=pt2s(p2,t2)
 
 h3=ps2h(p2,s11)
-hdis13=h11-h3   # 等熵焓降
-hd112=h11-h2     # 过程焓降
+hdis13=h11-h3   # 绛夌喌鐒撻檷
+hd112=h11-h2     # 杩囩▼鐒撻檷
 
 ieff=100*hd112/hdis13
 
 h4=ps2h(p2,s10)
-hdis14=h10-h4   # 等熵焓降
-hd102=h10-h2     # 过程焓降
+hdis14=h10-h4   # 绛夌喌鐒撻檷
+hd102=h10-h2     # 杩囩▼鐒撻檷
 
 eeff=100*hd102/hdis14
 
@@ -51,7 +50,7 @@ def linepoint3(sstep,p,t,h,s,sis=None):
     if sis==None:
         s0 = s - sstep
     else:
-        s0=sis-sstep  
+        s0=sis-sstep
     s2 = s + sstep
     point_h = np.zeros(shape=3)
     point_h[0] = ps2h(p, s0)
@@ -72,19 +71,19 @@ def linepoint2(h0,h1,s0,s1):
     point_s[1] = s1
     return point_s,point_h
 
-# p10等压
+# p10绛夊帇
 point_p10_s,point_p10_h=linepoint3(0.02,p10,t10,h10,s10)
-# p11等压
+# p11绛夊帇
 point_p11_s,point_p11_h=linepoint3(0.02,p11,t11,h11,s11)
 
-# p2等压线
+# p2绛夊帇绾�
 point_p2_s,point_p2_h=linepoint3(0.02,p2,t2,h2,s2,s11)
 
-# p11-p2等熵降线
+# p11-p2绛夌喌闄嶇嚎
 point_is_s11_2,point_is_h11_2=linepoint2(h11,h3,s11,s11)
 # p11-p2 Expansion Line
 point_hp_s11_2,point_hp_h11_2=linepoint2(h11,h2,s11,s2)
-# p10-p2等熵降线
+# p10-p2绛夌喌闄嶇嚎
 point_is_s10_2,point_is_h10_2=linepoint2(h10,h4,s10,s10)
 # p10-p2 Expansion Line
 point_hp_s10_2,point_hp_h10_2=linepoint2(h10,h2,s10,s2)
@@ -101,10 +100,10 @@ plt.plot(point_hp_s10_2,point_hp_h10_2,'rs-')
 
 plt.minorticks_on()
 
-_title =( 'The internal efficiency = ' + 
+_title =( 'The internal efficiency = ' +
     r'$\frac{h11-h2}{h11-h3}$' + '=' + '{:.2f}'.format(ieff) + '%'
           +'\n'+
-          'The external efficiency = ' + 
+          'The external efficiency = ' +
     r'$\frac{h10-h2}{h10-h4}$' + '=' + '{:.2f}'.format(eeff) + '%')
 plt.legend(loc="center left", bbox_to_anchor=[0.5, 0.5],
         ncol=2, shadow=True, title= _title)
@@ -137,4 +136,3 @@ plt.annotate('H4',
 plt.xlabel('s')
 plt.ylabel('h')
 plt.show()
-
