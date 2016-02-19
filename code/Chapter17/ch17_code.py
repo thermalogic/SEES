0492e41a3c6a684e821e3e3a28c36d32245b1061
hydro <hydro@Hydro>
2016-02-19 14:41:19 +0800

    update ch17

---
+++ code/Chapter17/ch17_code.py
@@ -0,0 +1,233 @@
+import pylab, random
+from rcParamsSettings import *
+
+#Page 236, Figure 17.2
+class Item(object):
+    def __init__(self, n, v, w):
+        self.name = n
+        self.value = float(v)
+        self.weight = float(w)
+    def getName(self):
+        return self.name
+    def getValue(self):
+        return self.value
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        result = '<' + self.name + ', ' + str(self.value)\
+                 + ', ' + str(self.weight) + '>'
+        return result
+
+def value(item):
+    return item.getValue()
+
+def weightInverse(item):
+    return 1.0/item.getWeight()
+
+def density(item):
+    return item.getValue()/item.getWeight()
+
+def buildItems():
+    names = ['clock', 'painting', 'radio', 'vase', 'book', 'computer']
+    values = [175,90,20,50,10,200]
+    weights = [10,9,4,2,1,20]
+    Items = []
+    for i in range(len(values)):
+        Items.append(Item(names[i], values[i], weights[i]))
+    return Items
+
+#Page 237, Figure 17.3
+def greedy(items, maxWeight, keyFunction):
+    """Assumes Items a list, maxWeight >= 0,
+         keyFunction maps elements of Items to floats"""
+    itemsCopy = sorted(items, key=keyFunction, reverse = True)
+    result = []
+    totalValue = 0.0
+    totalWeight = 0.0
+    for i in range(len(itemsCopy)):
+        if (totalWeight + itemsCopy[i].getWeight()) <= maxWeight:
+            result.append(itemsCopy[i])
+            totalWeight += itemsCopy[i].getWeight()
+            totalValue += itemsCopy[i].getValue()
+    return (result, totalValue)
+
+def testGreedy(items, constraint, keyFunction):
+    taken, val = greedy(items, constraint, keyFunction)
+    print('Total value of items taken = ', val)
+    for item in taken:
+        print( '   ', item)
+
+def testGreedys(maxWeight = 20):
+    items = buildItems()
+    print ('Use greedy by value to fill knapsack of size', maxWeight)
+    testGreedy(items, maxWeight, value)
+    print ('\nUse greedy by weight to fill knapsack of size', maxWeight)
+    testGreedy(items, maxWeight, weightInverse)
+    print( '\nUse greedy by density to fill knapsack of size', maxWeight)
+    testGreedy(items, maxWeight, density)
+
+#Page 239, Figure 17.4
+def chooseBest(pset, maxWeight, getVal, getWeight):
+    bestVal = 0.0
+    bestSet = None
+    for items in pset:
+        itemsVal = 0.0
+        itemsWeight = 0.0
+        for item in items:
+            itemsVal += getVal(item)
+            itemsWeight += getWeight(item)
+        if itemsWeight <= maxWeight and itemsVal > bestVal:
+            bestVal = itemsVal
+            bestSet = items
+    return (bestSet, bestVal)
+
+def testBest(maxWeight = 20):
+    items = buildItems()
+    pset = genPowerset(items)
+    taken, val = chooseBest(pset, maxWeight, Item.getValue,
+                            Item.getWeight)
+    print('Total value of items taken =', val)
+    for item in taken:
+        print(item)
+
+#Page 242, Figure 17.5
+class Node(object):
+    def __init__(self, name):
+        """Assumes name is a string"""
+        self.name = name
+    def getName(self):
+        return self.name
+    def __str__(self):
+        return self.name
+
+class Edge(object):
+    def __init__(self, src, dest):
+        """Assumes src and dest are nodes"""
+        self.src = src
+        self.dest = dest
+    def getSource(self):
+        return self.src
+    def getDestination(self):
+        return self.dest
+    def __str__(self):
+        return self.src.getName() + '->' + self.dest.getName()
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight = 1.0):
+        """Assumes src and dest are nodes, weight a float"""
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+    def getWeight(self):
+        return self.weight
+    def __str__(self):
+        return self.src.getName() + '->(' + str(self.weight) + ')'\
+               + self.dest.getName()
+
+#Page 243, Figure 17.6
+class Digraph(object):
+    #nodes is a list of the nodes in the graph
+    #edges is a dict mapping each node to a list of its children
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError('Duplicate node')
+        else:
+            self.nodes.append(node)
+            self.edges[node] = []
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not(src in self.nodes and dest in self.nodes):
+            raise ValueError('Node not in graph')
+        self.edges[src].append(dest)
+    def childrenOf(self, node):
+        return self.edges[node]
+    def hasNode(self, node):
+        return node in self.nodes
+    def __str__(self):
+        result = ''
+        for src in self.nodes:
+            for dest in self.edges[src]:
+                result = result + src.getName() + '->'\
+                         + dest.getName() + '\n'
+        return result[:-1] #omit final newline
+
+class Graph(Digraph):
+    def addEdge(self, edge):
+        Digraph.addEdge(self, edge)
+        rev = Edge(edge.getDestination(), edge.getSource())
+        Digraph.addEdge(self, rev)
+
+#Page 248, Figure 17.8
+def printPath(path):
+    """Assumes path is a list of nodes"""
+    result = ''
+    for i in range(len(path)):
+        result = result + str(path[i])
+        if i != len(path) - 1:
+            result = result + '->'
+    return result 
+
+def DFS(graph, start, end, path, shortest):
+    """Assumes graph is a Digraph; start and end are nodes;
+          path and shortest are lists of nodes
+       Returns a shortest path from start to end in graph"""
+    path = path + [start]
+    print('Current DFS path:', printPath(path))
+    if start == end:
+        return path
+    for node in graph.childrenOf(start):
+        if node not in path: #avoid cycles
+            if shortest == None or len(path) < len(shortest):
+                newPath = DFS(graph, node, end, path, shortest)
+                if newPath != None:
+                    shortest = newPath
+    return shortest
+
+def search(graph, start, end):
+    """Assumes graph is a Digraph; start and end are nodes
+       Returns a shortest path from start to end in graph"""
+    return DFS(graph, start, end, [], None)
+
+#Page 248, Figure 17.9
+def testSP():
+    nodes = []
+    for name in range(6): #Create 6 nodes
+        nodes.append(Node(str(name)))
+    g = Digraph()
+    for n in nodes:
+        g.addNode(n)
+    g.addEdge(Edge(nodes[0],nodes[1]))
+    g.addEdge(Edge(nodes[1],nodes[2]))
+    g.addEdge(Edge(nodes[2],nodes[3]))
+    g.addEdge(Edge(nodes[2],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[4]))
+    g.addEdge(Edge(nodes[3],nodes[5]))
+    g.addEdge(Edge(nodes[0],nodes[2]))
+    g.addEdge(Edge(nodes[1],nodes[0]))
+    g.addEdge(Edge(nodes[3],nodes[1]))
+    g.addEdge(Edge(nodes[4],nodes[0]))
+    sp = search(g, nodes[0], nodes[5])
+    print('Shortest path found by DFS:', printPath(sp))
+
+#Page 250, Figure 17.10
+def BFS(graph, start, end):
+    """Assumes graph is a Digraph; start and end are nodes
+       Returns a shortest path from start to end in graph"""
+    initPath = [start]
+    pathQueue = [initPath]
+    while len(pathQueue) != 0:
+        #Get and remove oldest element in pathQueue
+        tmpPath = pathQueue.pop(0)
+        print('Current BFS path:', printPath(tmpPath))
+        lastNode = tmpPath[-1]
+        if lastNode == end:
+            return tmpPath
+        for nextNode in graph.childrenOf(lastNode):
+            if nextNode not in tmpPath:
+                newPath = tmpPath + [nextNode]
+                pathQueue.append(newPath)
+    return None
