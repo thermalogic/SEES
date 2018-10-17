4ca9911a0b7c0258975197393a4a7dbe564a0926
thermalogic <cmh@seu.edu.cn>
2018-10-18 07:52:40 +0800

    update S2019

---
+++ notebook/code/gcc/cppdemo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+ 
+int main ()
+{
+   int x = 12;
+   if( x % 2==0 )
+   {
+       cout << "Even" <<endl; 
+       cout <<x<<endl; 
+       cout <<x%2<<endl; 
+   }
+   else
+   { 
+       cout << "Odd" << endl;
+   };
+   return 0;
+}
