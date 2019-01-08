3c48ed9fe6214fb5403e41de87b271c8d48e1471
thermalogic <cmh@seu.edu.cn>
2019-01-08 16:21:49 +0800

    update googletest

--- notebook/demo/src/test4_googletest.cpp
+++ notebook/demo/src/test4_googletest.cpp
@@ -1,39 +1,34 @@
 
 #include "region4.h"
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 using ::testing::InitGoogleTest;
 
-namespace {
-
 //  T,p
-double tab35[3][2] = {
-    {300, 0.00353658941},
-    {500, 2.63889776},
-    {600, 12.3443146}};
+double tab35[3][2] = {{300, 0.00353658941},
+                      {500, 2.63889776},
+                      {600, 12.3443146}};
 // p,T
-double tab36[3][2] = {
-    {0.1, 372.755919},
-    {1, 453.035632},
-    {10, 584.149488}};
- 
+double tab36[3][2] = {{0.1, 372.755919},
+                      {1, 453.035632},
+                      {10, 584.149488}};
+
 // Test Region4: Saturation line
-TEST(Region4Test,SaturationlineTest) {
-  
+TEST(Region4Test, SaturationlineTest)
+{
   for (int i = 0; i < 3; i++)
   {
     double T = tab35[i][0];
-    EXPECT_NEAR(tab35[i][1], pSat(T),1.0e-6);
+    EXPECT_NEAR(tab35[i][1], pSat(T), 1.0e-6);
   }
   for (int i = 0; i < 3; i++)
   {
     double p = tab36[i][0];
-    EXPECT_NEAR(tab36[i][1], TSat(p),1.0e-6);
+    EXPECT_NEAR(tab36[i][1], TSat(p), 1.0e-6);
   }
 }
 
-}  // namespace
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
