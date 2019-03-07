24636fb57eef9de2f0053f9c7594953cdf0745ef
thermalogic <cmh@seu.edu.cn>
2019-03-07 19:03:56 +0800

    modified Unit1-2-02(dir,fig)

---
+++ notebook/code/gcc/cpp_clang.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+   cout << "Yankees rule!" <<endl; 
+   cout <<"But not in Boston!"<<endl; 
+   cout <<"Yankees rule, "<<"but not in Boston"<<endl; 
+   return 0;
+}
