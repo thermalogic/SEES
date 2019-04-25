a00f31e39b92ec2f30308ad8a99abe298a0c9f44
thermalogic <cmh@seu.edu.cn>
2019-04-25 17:19:01 +0800

    04.25

---
+++ notebook/code/unittest/unittest_simple_more_detailed_information.py
@@ -0,0 +1,13 @@
+
+import unittest
+
+class SimplisticTest(unittest.TestCase):
+
+    def test_true(self):
+        self.assertTrue(True)
+ 
+    def test(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
