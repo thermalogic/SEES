1a4436ad26cf4e43270c093f23df9cb8d95dfe26
thermalogic <cmh@seu.edu.cn>
2019-04-29 21:11:59 +0800

    update 04.29

---
+++ notebook/code/unittest/unittest_almostequal.py
@@ -0,0 +1,17 @@
+import unittest
+
+class AlmostEqualTest(unittest.TestCase):
+
+    def test_NotAlmostEqual(self):
+        self.assertNotAlmostEqual(1.11, 1.3, places=1)
+
+    def test_AlmostEqual(self):
+       # self.assertAlmostEqual(1.1, 1.3, places=0)
+        self.assertAlmostEqual(0.12345678, 0.12345679) 
+
+    def test_AlmostEqualWithDEfault(self):
+        self.assertNotAlmostEqual(1.1, 1.3)
+        
+
+if __name__ == '__main__':
+    unittest.main()
