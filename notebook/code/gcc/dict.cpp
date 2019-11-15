b26c58bfab9bca0c4b6dd7fefd5c422ecd4acaa8
thermalogic <cmh@seu.edu.cn>
2019-11-15 21:55:10 +0800

    add  C++ equivalent of Python dict

---
+++ notebook/code/gcc/dict.cpp
@@ -0,0 +1,16 @@
+#include <unordered_map>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+   unordered_map<string, int> dishes = {{"eggs",  2}, {"sausage", 1},{ "bacon", 1 }, {"spam", 500}};
+   cout << dishes["eggs"] << endl; 
+   typedef unordered_map<string,int> dictstrint;
+   for(  dictstrint::iterator it= dishes.begin();it!= dishes.end();it++ ){
+        cout<<"key = "<<it->first<<" value = "<<it->second<<endl;
+   }
+   return 0; 
+}
