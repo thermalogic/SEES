060b2ef100b26bc28c4e19b0586116f60dcf3f98
thermalogic <cmh@seu.edu.cn>
2019-11-23 16:19:35 +0800

    refining

--- notebook/demo/src/DemoPolynomialFit.c
+++ notebook/demo/src/DemoPolynomialFit.c
@@ -1,26 +1,74 @@
 /*
    The Demo of Simple PolynomialFit 
 */
-#include <stdio.h>
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 void PolynomialFit(double x[], double y[], int size, int n, double a[]);
 
 int main()
 {
-    int N = 11;
-    double x[11] = {10.0, 8.0, 13.0, 9.0, 11.0, 14.0, 6.0, 4.0, 12.0, 7.0, 5.0};
-    double y[11] = {8.04, 6.95, 7.68, 8.81, 8.33, 9.96, 7.24, 4.26, 10.84, 4.82, 5.68};
-    int n = 1; // n is the degree of Polynomial
-    double a[n + 1];
-    int size = sizeof(x) / sizeof(double);
-    PolynomialFit(x, y, size, n, a);
-    printf("\nThe values of the coefficients are as follows:\n");
-    for (int i = 0; i <= n; i++)
-        printf("x^ %d = %.4f\n", i, a[i]);
-    printf("\nHence the fitted Polynomial is given by:\ny=");
-    for (int i = 0; i <= n; i++)
-        printf("+( %.4f ) x^ %d", a[i], i);
-    printf("\n");
-    return 0;
+
+  int MAX_LINE_SIZE = 25;
+  char file_name[] = "./data/springData.csv";
+  FILE *fp;
+  fp = fopen(file_name, "r");
+
+  if (!fp)
+  {
+    fprintf(stderr, "failed to open file for reading\n");
+    return 1;
+  }
+
+  char line[MAX_LINE_SIZE];
+  char *result = NULL;
+  int size = 19;
+  double distances[size];
+  double masses[size];
+  double forces[size];
+
+  int ix = 0;
+  int iy = 0;
+
+  while (fgets(line, MAX_LINE_SIZE, fp) != NULL)
+  {
+
+    result = strtok(line, ",");
+    int i = 0;
+    while (result != NULL)
+    {
+      if (i != 0)
+      {
+        if (atof(result) > 0)
+        {
+          masses[ix] = atof(result);
+          ix++;
+        }
+      }
+      else
+      {
+        if (atof(result) > 0)
+        {
+          distances[iy] = atof(result);
+          iy++;
+        }
+        i++;
+      }
+      result = strtok(NULL, ",");
+    }
+  }
+  fclose(fp);
+
+  for (int n = 0; n < size; n++)
+  {
+    forces[n] = masses[n] * 9.81;
+  }
+
+  int n = 1; // n is the degree of Polynomial
+  double a[n + 1];
+  PolynomialFit(forces, distances, size - 6, n, a);
+  printf(" PolynomialFit: k = %.2f", 1 / a[1]);
+  return 0;
 }
