cab0a270919595e3c9e246914eddd56cecba8930
thermalogic <cmh@seu.edu.cn>
2020-12-10 21:38:54 +0800

    vpr ok!

--- notebook/refrigeration/components/node.py
+++ notebook/refrigeration/components/node.py
@@ -3,8 +3,8 @@ import CoolProp.CoolProp as cp
 
 class Node:
 
-    title = ('{:^6} \t {:^30} \t {:<3}\t {:>10}\t {:>10}\t {:>10} \t {:>10}\t {:>5}\t {:>15}'.format
-             ("NodeID", "Name", "P(MPa)", "T(°C)", "H(kJ/kg)", "S(kJ/kg.K)", "V(m^3/kg)", "X", "MDOT(kg/s)"))
+    title = ('{:^6} \t {:^30} \t {:<3}\t {:>10}\t {:>10}\t {:>10} \t {:>5}\t {:>15}'.format
+             ("NodeID", "Name", "P(MPa)", "T(°C)", "H(kJ/kg)", "S(kJ/kg.K)",  "X", "MDOT(kg/s)"))
 
     def __init__(self, dictnode):
         """ create the node object"""
@@ -29,7 +29,6 @@ class Node:
         self.p = None
         self.h = None
         self.s = None
-        self.v = None
         self.stateok = False
 
         if self.t is not None and self.x is not None:
@@ -40,16 +39,14 @@ class Node:
                             'Q', self.x, 'R134a')/1.0e6
         self.h = cp.PropsSI('H', 'T', 273.15+self.t, 'Q', self.x, 'R134a')/1000
         self.s = cp.PropsSI('S', 'T', 273.15+self.t, 'Q', self.x, 'R134a')/1000
-        self.v = cp.PropsSI('V', 'T', 273.15+self.t, 'Q', self.x, 'R134a')/1000
         self.stateok = True
 
     def ps(self):
-        self.h = cp.PropsSI('H', 'P', self.p*1.0e6, 'S',self.s*1000, 'R134a')/1000
-        self.t = cp.PropsSI('T', 'P', self.p*1.0e6, 'S',self.s*1000, 'R134a')-273.15
-        self.x = cp.PropsSI('Q', 'P', self.p*1.0e6, 'S',self.s*1000, 'R134a')/1000
-        self.v = cp.PropsSI('V', 'P', self.p*1.0e6, 'S', self.s*1000, 'R134a')/1000
+        self.h = cp.PropsSI('H', 'P', self.p*1.0e6, 'S',
+                            self.s*1000, 'R134a')/1000
+        self.t = cp.PropsSI('T', 'P', self.p*1.0e6, 'S',
+                            self.s*1000, 'R134a')-273.15
         self.stateok = True
-
 
     def setstate(self):
         if self.t is not None and self.x is not None:
@@ -58,45 +55,20 @@ class Node:
             self.ps()
 
     def __str__(self):
-        try:
-            result = ('{:^6} \t {:<30} \t {:>6.3}\t {:>10.2f}\t {:>10.2f}\t {:>5.2f} \t {:>15.3f}\t {:>5.3}\t {:>12.2f}'.format
-                      (self.id, self.name, self.p, self.t, self.h, self.s, self.v, self.x, self.mdot))
-        except:
-            result = ('{:^6} \t {:<30}'.format(self.id, self.name))
-            
-            if self.p is not None:
-                result +=('\t {:<6.3}'.format(self.p))
-            else:
-                result += ('\t {:<6}'.format(""))
-            
-            if self.t is not None:
-                result +=('\t {:>10.2f}'.format(self.t))
-            else:
-                result += ('\t {:>10}'.format(""))
+        result = ('{:^6} \t {:<30}'.format(self.id, self.name))
 
-            if self.h is not None:
-                result +=('\t {:>10.2f}'.format(self.h))
-            else:
-                result += ('\t {:>10}'.format(""))
+        OutStrs = [{"fstr": '\t {:<6.3}', "sstr": '\t {:<6}', 'prop': self.p},
+                   {"fstr": '\t {:>10.2f}', "sstr": '\t {:>10}', 'prop': self.t},
+                   {"fstr": '\t {:>10.2f}', "sstr": '\t {:>10}', 'prop': self.h},
+                   {"fstr": '\t {:>7.2f}', "sstr": '\t {:>7}', 'prop': self.s},
+                   {"fstr": '\t {:>10.2f}', "sstr": '\t {:>10}', 'prop': self.x},
+                   {"fstr": '\t {:>12.2f}', "sstr": '\t {:>12}', 'prop': self.mdot}
+                   ]
 
-            if self.s is not None:
-                result +=('\t {:>5.2f}'.format(self.s))
-            else:
-                result += ('\t {:>5}'.format(""))
-                
-            if self.v is not None:
-                result +=('\t {:>15.3f}'.format(self.v))
-            else:
-                result += ('\t {:>15}'.format(""))
-            
-            if self.x is not None:
-                result +=('\t {:>5.3f}'.format(self.x))
-            else:
-                result += ('\t {:>5}'.format(""))
-            
-            if self.mdot is not None:
-                result +=('\t {:>12.2f}'.format(self.mdot))
-            else:
-                result += ('\t {:>12}'.format(""))
-        # string   
+        for item in OutStrs:
+            try:
+                result += item["fstr"].format(item["prop"])
+            except:
+                result += item["sstr"].format("")
+
         return result
