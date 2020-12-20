2655d423719283aa1a252bcc53681e4675d2e267
thermalogic <cmh@seu.edu.cn>
2020-12-20 09:06:55 +0800

    update ModernCPP

--- notebook/vcrcpp/demonode.cpp
+++ notebook/vcrcpp/demonode.cpp
@@ -26,13 +26,14 @@ int main()
 
   Node *curnode1;
   curnode1 = new Node(dictnode1);
-  cout << setiosflags(ios::fixed) << curnode1->desc << endl;
-  cout << setiosflags(ios::fixed) << curnode1->id << endl;
-  cout << setiosflags(ios::fixed) << curnode1->p << endl;
-  cout << setiosflags(ios::fixed) << curnode1->t << endl;
-  cout << setiosflags(ios::fixed) << curnode1->h << endl;
-  cout << setiosflags(ios::fixed) << curnode1->s << endl;
-  cout << setiosflags(ios::fixed) << curnode1->mdot << endl;
+  cout << setiosflags(ios::fixed); 
+  cout << curnode1->desc << endl;
+  cout << curnode1->id << endl;
+  cout << curnode1->p << endl;
+  cout << curnode1->t << endl;
+  cout << curnode1->h << endl;
+  cout << curnode1->s << endl;
+  cout << curnode1->mdot << endl;
 
   vector<dictNode> dictNodes = {
       {{"desc", "Evaporator to Compressor"},
@@ -57,13 +58,13 @@ int main()
 
   for (auto &item : curnodes)
   {
-    cout << setiosflags(ios::fixed) << item->desc << endl;
-    cout << setiosflags(ios::fixed) << item->id << endl;
-    cout << setiosflags(ios::fixed) << item->p << endl;
-    cout << setiosflags(ios::fixed) << item->t << endl;
-    cout << setiosflags(ios::fixed) << item->h << endl;
-    cout << setiosflags(ios::fixed) << item->s << endl;
-    cout << setiosflags(ios::fixed) << item->mdot << endl;
+    cout << item->desc << endl;
+    cout << item->id << endl;
+    cout << item->p << endl;
+    cout << item->t << endl;
+    cout << item->h << endl;
+    cout << item->s << endl;
+    cout << item->mdot << endl;
   }
   return 0;
 }
