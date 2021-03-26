985ffc24bd0357c2210cffc75516c2ecae14e235
thermalogic <cmh@seu.edu.cn>
2021-03-26 09:22:49 +0800

    add EE

---
+++ notebook/code/unittest/unittest_fixtures.py
@@ -0,0 +1,23 @@
+
+import unittest
+
+class FixturesTest(unittest.TestCase):
+
+    def setUp(self):
+        print('In setUp()')
+        self.fixture = range(1, 10)
+
+    def tearDown(self):
+        print('In tearDown()')
+        del self.fixture
+
+    def test_fixture1(self):
+        print('in test1()')
+        self.assertEqual(self.fixture, range(1, 10))
+     
+    def test_fixture2(self):
+        print('in test2()')
+        self.assertEqual(self.fixture, range(2, 10))
+
+if __name__ == '__main__':
+    unittest.main()
