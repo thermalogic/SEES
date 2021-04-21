3c3a333eb3da785d9623a20a1e644eaf838b83e3
thermalogic <cmh@seu.edu.cn>
2021-04-21 11:23:42 +0800

    update hash

--- notebook/demo/src/demo1_unordered_map.cpp
+++ notebook/demo/src/demo1_unordered_map.cpp
@@ -1,14 +1,16 @@
 
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <unordered_map>
  
+using namespace std;
+typedef tuple<string,string,float> tupTag;
+ 
 int main()
-{
-    std::unordered_map<std::string, int> months;
-    months["january"] = 31;
-    months["february"] = 28;
-    months["march"] = 31;
-    std::cout << "february  -> " << months["february"] << std::endl;
+{  
+    unordered_map<int, tupTag> tags;
+    tags[108600] =(tupTag){"CompressorIPortM","压缩机入口质量流量",0.08 };
+    cout << "108600:  " <<get<0>(tags[108600]) <<"\t"<< get<1>(tags[108600])<< "\t"<<get<2>(tags[108600])<<endl;
     return 0;
 }
