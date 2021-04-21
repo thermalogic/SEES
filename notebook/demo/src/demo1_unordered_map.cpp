5e24d360ab6eac8ad41a87e103ac79bd5cec1b26
thermalogic <cmh@seu.edu.cn>
2021-04-21 11:42:11 +0800

    refined

--- notebook/demo/src/demo1_unordered_map.cpp
+++ notebook/demo/src/demo1_unordered_map.cpp
@@ -5,12 +5,13 @@
 #include <unordered_map>
  
 using namespace std;
-typedef tuple<string,string,float> tupTag;
+typedef tuple<string,string,string,float> tupTag;
  
 int main()
 {  
     unordered_map<int, tupTag> tags;
-    tags[108600] =(tupTag){"CompressorIPortM","压缩机入口质量流量",0.08 };
-    cout << "108600:  " <<get<0>(tags[108600]) <<"\t"<< get<1>(tags[108600])<< "\t"<<get<2>(tags[108600])<<endl;
+    tags[108600] =(tupTag){"CompressorIPortM","压缩机入口质量流量","kg/s",0.08 };
+    cout << "Tag 108600:  " <<get<0>(tags[108600]) <<"\t"<< get<1>(tags[108600])
+         << "\t"<<get<2>(tags[108600])<< "\t"<<get<3>(tags[108600])<<endl;
     return 0;
 }
